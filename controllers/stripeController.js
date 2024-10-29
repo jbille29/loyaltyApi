c4f1ece77d34e71ace3477ff1fd9f84d7e151fd6
jbille29 <jbille29@gmail.com>
2024-10-29 11:06:48 -0500

    updated stripeConroller

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,11 +2,10 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const createCheckoutSession = async (req, res) => {
-  console.log('register route!')
-  res.send('hello from checkout')
-  /*
-  const { priceId, customerEmail } = req.body;
+  console.log('hellow from checkout')
   
+  const { priceId, customerEmail } = req.body;
+
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -21,12 +20,13 @@ const createCheckoutSession = async (req, res) => {
       success_url: `${process.env.CLIENT_URL}/dashboard?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.CLIENT_URL}/pricing`,
     });
-
+    console.log('its working')
     res.json({ sessionId: session.id });
   } catch (error) {
+    console.log('not working')
     res.status(500).json({ error: 'Failed to create checkout session' });
   }
-    */
+    
 };
 
 module.exports = { createCheckoutSession };
